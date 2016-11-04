22ac03f5aa0bbb13c378b9926ff0e4884ef99e25
Dorian Karter <dkarter@gmail.com>
2016-11-04 15:06:48 -0500

    Change socket name to treat-dispenser

--- server/puma.rb
+++ server/puma.rb
@@ -1,7 +1,7 @@
 root = Dir.getwd.to_s
 
 activate_control_app 'tcp://0.0.0.0:8080'
-bind 'unix:///tmp/puma.pumatra.sock'
+bind 'unix:///tmp/puma.treat-dispenser.sock'
 pidfile "#{root}/tmp/pids/puma.pid"
 rackup "#{root}/config.ru"
 state_path "#{root}/tmp/pids/puma.state"
